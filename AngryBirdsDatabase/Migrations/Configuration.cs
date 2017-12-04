9d81b5ee02ebbe3599daf4ae9699fba744a0012a
Molyn Eriksson <molyneriksson@gmail.com>
2017-12-04 16:12:54 +0100

    La player och level objekt utanför seed metoden

--- AngryBirdsDatabase/Migrations/Configuration.cs
+++ AngryBirdsDatabase/Migrations/Configuration.cs
@@ -7,6 +7,9 @@ namespace AngryBirdsDatabase.Migrations
 
     internal sealed class Configuration : DbMigrationsConfiguration<AngryBirdsDatabase.MyContext>
     {
+        Player player = new Player { PlayerName = "Camilla", PlayerKey = 1 };
+        Level level = new Level { Birds = 7, LevelNumber = 1 };
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
@@ -16,21 +19,20 @@ namespace AngryBirdsDatabase.Migrations
         protected override void Seed(AngryBirdsDatabase.MyContext context)
         {   
 
-            context.Players.AddOrUpdate<Player> (new Player {PlayerName = "Camilla", PlayerKey = 1});
-            var player1 = context.Players.Where(p => p.PlayerKey == 1).First();
-        
-            context.Levels.AddOrUpdate<Level> (new Level { Birds = 7, LevelNumber = 1});
-            var level1 = context.Levels.Where(l => l.LevelKey == 1).First();
+            context.Players.AddOrUpdate<Player> (player);
 
-            context.Scores.AddOrUpdate<Score>(new Score { LevelScore = 1000, BirdsLeft = 2, Player = player1, Level = level1});
-            context.SaveChanges();
-            
-            
+
+            context.Levels.AddOrUpdate<Level>(level);
+
+            context.Scores.AddOrUpdate<Score>(new Score { LevelScore = 1000, BirdsLeft = 2, Player = player, Level = level });
+
+
+
 
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-          }
+        }
     }
 }
